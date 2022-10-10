607251a1ab979b7d0dab24338eec5823990c8245
Kevin Lucido <112150217+kevinjoshualucido@users.noreply.github.com>
2022-10-09 23:54:09 -0500

    Add files via upload

---
+++ users_cr/users_schema.sql
@@ -0,0 +1,4 @@
+SELECT * FROM users;
+
+# INSERT INTO users.users (first_name, last_name, email);
+# VALUES ("Kyrie", "Irving", "kyrieirving@gmail.com"), ("Kevin", "Love", "kevinlove@gmail.com"), ("George", "Russell", "grussell63@gmail.com");
