06af0cb142c69128698970ec89b7043b13aee9d2
Kevin Lucido <112150217+kevinjoshualucido@users.noreply.github.com>
2022-10-30 13:34:55 -0500

    Delete users_cr directory

--- users_cr/users.sql
+++
@@ -1,4 +0,0 @@
-# SELECT * FROM users;
-
-# INSERT INTO users.users (first_name, last_name, email);
-# VALUES ("Kyrie", "Irving", "kyrieirving@gmail.com"), ("Kevin", "Love", "kevinlove@gmail.com"), ("George", "Russell", "grussell63@gmail.com");
