d7dffde4ba83763478f6f465673bfec868cc106f
Kevin Lucido <112150217+kevinjoshualucido@users.noreply.github.com>
2022-10-30 13:35:03 -0500

    Delete users_full_crud directory

--- users_full_crud/users_schema.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM users;
-
-# INSERT INTO users_schema.users (first_name, last_name, email)
-# VALUES ("Johnny", "Galeki", "jgaleki@gmail.com"), ("Kaley", "Cuoco-Sweeting", "thisiskaley10@gmail.com"), ("Jim", "Parsons", "jimparsons@icloud.com");
